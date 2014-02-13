12dc87bf6c5e187d85d5e0ef4ece17c6d3e9c9b9
ProWebCraft <prowebcraft@gmail.com>
2014-02-13 10:20:33 +0400

    Updated cart actions & effects

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -1,43 +1,122 @@
-$(document).ready(function(){
-    $(".models-container").find('li button.btn-cart').click(function(){
+$(document).ready(function () {
+    $(".models-container").find('li button.btn-cart').click(function () {
         var $item = $(this).closest('li'),
             itemID = $item.data('id'),
-            $img = $item.find('img'),
+            price = parseInt($item.find('div.price').text());
+        $img = $item.find('img').first(),
             $cart = $('.shopping-cart');
-        var imgtodrag = $(this).parent('.item').find("img").eq(0);
-        if (imgtodrag) {
-            var imgclone = imgtodrag.clone()
+        if ($img.length) {
+            var $imgClone = $img.clone()
                 .offset({
-                    top: imgtodrag.offset().top,
-                    left: imgtodrag.offset().left
+                    top: $img.offset().top,
+                    left: $img.offset().left
                 })
                 .css({
-                    'opacity': '0.5',
+                    'opacity': '0.8',
                     'position': 'absolute',
-                    'height': '150px',
-                    'width': '150px',
+                    'width': $img.outerWidth() + "px",
+                    'height': $img.outerHeight() + "px",
                     'z-index': '100'
                 })
                 .appendTo($('body'))
                 .animate({
-                    'top': cart.offset().top + 10,
-                    'left': cart.offset().left + 10,
-                    'width': 75,
-                    'height': 75
-                }, 1000, 'easeInOutExpo');
+                    'top': $cart.offset().top,
+                    'left': $cart.offset().left,
+                    'width': "50px",
+                    'height': "50px"
+                }, 500, 'easeInOutCubic');
 
             setTimeout(function () {
-                cart.effect("shake", {
-                    times: 2
-                }, 200);
-            }, 1500);
+                $imgClone.detach();
+                $cart.stop().fadeOut('fast', function () {
+                    addToCart(itemID, price);
+                    $cart.stop().fadeIn();
+                });
+            }, 800);
 
-            imgclone.animate({
+            /*
+            $imgClone.animate({
                 'width': 0,
                 'height': 0
             }, function () {
                 $(this).detach()
             });
+            */
         }
     });
+
+    $(".shopping-cart").find('.cart-clear').click(function(e){
+        e.preventDefault();
+        var $cart = $('.shopping-cart');
+        localStorage.removeItem('cart');
+        $cart.fadeOut(function(){
+            updateCartState();
+            $cart.fadeIn();
+        });
+        return false;
+    });
+
+    updateCartState();
 });
+
+function updateCartState(cart) {
+
+    var cart = cart || getCart(),
+        $cart = $('.shopping-cart');
+    if (cart.length) {
+        var cartSum = 0;
+        for (var i = 0; i < cart.length; i++) {
+            cartSum += cart[i][1];
+        }
+        $cart.addClass('active').removeAttr('disabled');
+        $cart.find('.cart-count').text(cart.length);
+        $cart.find('.cart-sum').text(cartSum.formatMoney(2, ' ', ',', ' руб.'));
+    } else {
+        $cart.removeClass('active').attr('disabled', 'disabled');
+        $cart.find('.cart-count').text(0);
+        $cart.find('.cart-sum').text('');
+    }
+}
+
+function getCart() {
+    var cart = localStorage.getItem('cart');
+    return (cart && (cart = JSON.parse(cart)) ? cart : []);
+}
+
+function saveCart(cart) {
+    if (!cart) localStorage.removeItem('cart');
+    localStorage.setItem('cart', JSON.stringify(cart));
+    updateCartState();
+}
+
+function removeFromCart(id) {
+    var cart = getCart();
+    for (var i = 0; i < cart.length; i++) {
+        if (cart[i][0] == id)
+            cart.splice(i);
+    }
+    saveCart(cart);
+    return cart;
+}
+
+function addToCart(id, price) {
+    var cart = getCart();
+    cart.push([ id, price ]);
+    saveCart(cart);
+    return cart;
+}
+
+Number.prototype.formatMoney = function(decPlaces, thouSeparator, decSeparator, currencySymbol) {
+    // check the args and supply defaults:
+    decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces;
+    decSeparator = decSeparator == undefined ? "." : decSeparator;
+    thouSeparator = thouSeparator == undefined ? "," : thouSeparator;
+    currencySymbol = currencySymbol == undefined ? "$" : currencySymbol;
+
+    var n = this,
+        sign = n < 0 ? "-" : "",
+        i = parseInt(n = Math.abs(+n || 0).toFixed(decPlaces)) + "",
+        j = (j = i.length) > 3 ? j % 3 : 0;
+
+    return sign + (j ? i.substr(0, j) + thouSeparator : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thouSeparator) + (decPlaces ? decSeparator + Math.abs(n - i).toFixed(decPlaces).slice(2) : "") + currencySymbol;
+};
